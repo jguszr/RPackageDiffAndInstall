85350ee750db38e6510e784be2e2481765f1cf96
Gus <jguszr@gmail.com>
2017-04-18 21:15:31 -0300

    initial working version

--- diffInstall.R
+++ diffInstall.R
@@ -10,3 +10,9 @@ current <- as.data.frame(installed.packages()[,c(1,3:4)])
 rownames(current) <- NULL
 
 current <- current[is.na(current$Priority),1:2,drop=FALSE]
+
+#Filtering 
+to_be <- subset(to_be, !(to_be$Package %in% current$Package))
+
+# the Actual installation process
+lapply(to_be,install.packages)
